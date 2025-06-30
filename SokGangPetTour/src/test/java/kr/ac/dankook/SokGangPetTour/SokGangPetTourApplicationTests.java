e32cfec66a27b002772fb9b81669d3cdab53cfcc
ohchanKyu <okc0202@naver.com>
2025-06-30 20:46:04 +0900

    feat: Initial Spring boot.

---
+++ SokGangPetTour/src/test/java/kr/ac/dankook/SokGangPetTour/SokGangPetTourApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.SokGangPetTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokGangPetTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

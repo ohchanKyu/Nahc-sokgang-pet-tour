6b06bf5daebb509837231cc7a4ad1337e864c82d
ohchanKyu <okc0202@naver.com>
2025-08-22 12:31:08 +0900

    refactor: Refactor tour sync service logic.

---
+++ SokGangPetTour/src/main/java/kr/ac/dankook/SokGangPetTour/dto/request/tourRequest/TourOverviewRequest.java
@@ -0,0 +1,12 @@
+package kr.ac.dankook.SokGangPetTour.dto.request.tourRequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class TourOverviewRequest {
+    private String overview;
+}
